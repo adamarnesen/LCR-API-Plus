4f7da9af19efca77d1e4eb5b2d2d9dc915c7710b
Adam Arnesen <adamarnesen@gmail.com>
2024-10-21 22:12:50 -0500

    add notebook support and some upgrades for pandas (#15)
    * add notebook support
    
    * ignore ward report

--- run_analytics.py
+++ run_analytics.py
@@ -1,8 +1,12 @@
 import json
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
 from analytics.data import *
 from analytics.stake_quarterlies import create_quarterly_analytics
-from lcr import unit, quarterly_report
-from lcr.api import API
+from lcr import quarterly_report, unit
+from lcr.api import API, CHROME_OPTIONS
 
 
 def load_profile():
@@ -11,13 +15,30 @@ def load_profile():
     return profile
 
 
-def download_units_data(profile):
+def setup_api_from_profile(profile) -> API:
+    if profile["chrome_driver_path"]:
+        driver = webdriver.Chrome(
+            service=Service(profile["chrome_driver_path"]), options=CHROME_OPTIONS
+        )
+        api = API(
+            profile["username"],
+            profile["password"],
+            profile["unit_number"],
+            driver=driver,
+        )
+    else:
+        api = API(profile["username"], profile["password"], profile["unit_number"])
+    return api
+
+
+def download_units_data(profile) -> str:
+    """Downloads the units data based on units listed in the `profile`."""
     units = unit.load_units(profile["units"])
-    api = API(profile["username"], profile["password"], profile["unit_number"])
+    api = setup_api_from_profile(profile)
     reporter = quarterly_report.HistoricalQuarterlyReport(api, units)
-    reporter.download_historical_quarters_to_csv(
-        units, create_and_get_output_path(f"{profile['unit_name']}.csv")
-    )
+    output_file = create_and_get_output_path(f"{profile['unit_name']}.csv")
+    reporter.download_historical_quarters_to_csv(units, output_file)
+    return output_file
 
 
 def main():
